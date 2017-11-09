9b9682ff1720b2b447de7f86dbe56a1ba204b100
Ignacio Nicolás Negro Caino <nachonegrocaino@gmail.com>
2017-11-09 20:18:01 -0300

    shell first commit

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -1,16 +1,9 @@
 #include <syscall.h>
 #include <cuadraticGraficator.h>
 #include <lib.h>
+#include <shell.h>
 
 int main() {
-	int i = 20;
-	while(i > 0) {
-		char c = getchar();
-		write(&c,1,0x0F);
-		i--;
-	}
-	write("finish", 6, 0x0F);
-	// graphInit();
-	// graphCuadratic(2,1,0);
+	shell();
 	while(1);
 }
