5d60d199794a59dcecda0de38b3c5c4594bc6e0a
DonosoNaumczuk <adonoso@itba.edu.ar>
2017-11-09 01:42:28 -0300

    Keyboard circular buffer implemented and keyboard getchar function. Missing EOF key combination (Ctrl + D) and other key combinations e.g. (Ctrl + C).

--- Kernel/keyboard.c
+++ Kernel/keyboard.c
@@ -4,7 +4,7 @@
 unsigned int keycode_map[128] = {
     0,  27, '1', '2', '3', '4', '5', '6', '7', '8', /* INDEX: 0 - 9 */
     '9','0', '-', '=', BACKSPACE, '\t' /* Tab */,'q', 'w', 'e', 'r',	/* INDEX: 10 - 19 */
-  't', 'y', 'u', 'i', 'o', 'p', '[', ']', ENTER, 0 /* Control */, /* INDEX: 20 - 29 */
+  't', 'y', 'u', 'i', 'o', 'p', '[', ']', ENTER_KEY, 0 /* Control */, /* INDEX: 20 - 29 */
   'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* INDEX: 30 - 39 */
  '\'', '`',  LEFT_SHIFT,'\\', 'z', 'x', 'c', 'v', 'b', 'n', /* INDEX: 40 - 49 */
   'm', ',', '.', '/', LEFT_SHIFT,'*',0/* Alt */,' '/* Space bar */, CAPS_LOCK, 0 /* F1 */, /* INDEX: 50 - 59 */
@@ -17,14 +17,14 @@ unsigned int keycode_map[128] = {
 unsigned static char caps_lock = FALSE;
 unsigned static char left_shift = FALSE;
 unsigned static char right_shift = FALSE;
-#define ISLOWERCASE ((caps_lock && (left_shift || right_shift)) || (!caps_lock && !left_shift && !right_shift))
+static char buffer[MAX_BUFF_SIZE];
+static int current_index;
+static int end_index;
+static int bufferIsEmpty = TRUE;
 
 void keyboard_handler() {
 	unsigned char status;
 	char keycode;
-
-    /* End of interrupt, PIC ack */
-	write_port(0x20, 0x20);
 
 	status = read_port(KEYBOARD_STATUS_PORT);
 
@@ -32,25 +32,140 @@ void keyboard_handler() {
 	if(status & 0x01) {
 		keycode = read_port(KEYBOARD_DATA_PORT);
 
-        if(keycode_map[keycode] == RIGHT_SHIFT)
-            right_shift = TRUE;
-        else if(keycode_map[keycode] == LEFT_SHIFT)
-            left_shift = TRUE;
-        else if(keycode < 0 && keycode_map[keycode + MAXCODE] == RIGHT_SHIFT)
-            right_shift = FALSE;
-        else if(keycode < 0 && keycode_map[keycode + MAXCODE] == LEFT_SHIFT)
-            left_shift = FALSE;
-        else if(keycode_map[keycode] == CAPS_LOCK)
-            caps_lock = (caps_lock == TRUE)? FALSE : TRUE;
-        else if(keycode_map[keycode] == BACKSPACE)
-            deleteCurrent();
-        else if(keycode_map[keycode] == ENTER)
-            newLine();
-        else if(keycode >= 0) { /* Brake keycodes are < 0 */
-            if(ISLOWERCASE)
-                printCharWithColor(keycode_map[keycode],0x0F);
-            else
-                printCharWithColor(keycode_map[keycode] - 32,0x0F);
+        /* Brake codes are negative */
+        if(keycode < 0) {
+            if(keycode_map[keycode + MAXCODE] == RIGHT_SHIFT)
+                right_shift = FALSE;
+            else if(keycode_map[keycode + MAXCODE] == LEFT_SHIFT)
+                left_shift = FALSE;
+
+            return;
         }
-	}
+
+        unsigned int mapped_key = keycode_map[keycode];
+        if(mapped_key == RIGHT_SHIFT)
+            right_shift = TRUE;
+        else if(mapped_key == LEFT_SHIFT)
+            left_shift = TRUE;
+        else if(mapped_key == CAPS_LOCK)
+            caps_lock = (caps_lock == TRUE)? FALSE : TRUE;
+        else if(mapped_key == BACKSPACE)
+            add('\b');
+        else if(mapped_key == ENTER_KEY)
+            add('\n');
+        else if(isAlpha(mapped_key)) {
+            if(IS_LOWERCASE)
+                add(mapped_key);
+            else
+                add(shiftedChar(mapped_key));
+        }
+        else if(isNumber(mapped_key)) {
+            if(SHIFT_PRESSED)
+                add(shiftedChar(mapped_key));
+            else
+                add(mapped_key);
+        }
+    }
+}
+
+char shiftedChar(char c) {
+    char shifted;
+    if(isAlpha)
+        return c - 32;
+    switch (c) {
+        case '1':
+            shifted = '!';
+            break;
+        case '2':
+            shifted = '@';
+            break;
+        case '3':
+            shifted = '#';
+            break;
+        case '4':
+            shifted = '$';
+            break;
+        case '5':
+            shifted = '%';
+            break;
+        case '6':
+            shifted = '^';
+            break;
+        case '7':
+            shifted = '&';
+            break;
+        case '8':
+            shifted = '*';
+            break;
+        case '9':
+            shifted = '(';
+            break;
+        case '0':
+            shifted = ')';
+            break;
+        case '-':
+            shifted = '_';
+            break;
+        case '=':
+            shifted = '+';
+            break;
+        case '[':
+            shifted = '{';
+            break;
+        case ']':
+            shifted = '}';
+            break;
+        case '\'':
+            shifted = '"';
+            break;
+        case ';':
+            shifted = ':';
+            break;
+        case '/':
+            shifted = '?';
+            break;
+        case ',':
+            shifted = '<';
+            break;
+        case '.':
+            shifted = '>';
+            break;
+        default:
+            shifted = 0;
+    }
+    return shifted;
+}
+
+int isAlpha(char c) {
+    return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+}
+
+int isNumber(char c) {
+    return (c >= '0' && c <= '9');
+}
+
+int getchar() {
+    while (bufferIsEmpty);
+
+    int c = buffer[current_index];
+
+    if(++current_index > end_index) {
+        bufferIsEmpty = TRUE;
+    }
+
+    return c;
+}
+
+void add(char c) {
+    if(bufferIsEmpty) {
+        current_index = 0;
+        end_index = 0;
+        bufferIsEmpty = FALSE;
+    }
+    else {
+        end_index = (end_index + 1) % MAX_BUFF_SIZE;
+        if(end_index == current_index)
+            current_index++;
+    }
+    buffer[end_index] = c;
 }
