a07877d47cf5d40929c8e1166be86019afdcc108
AgustinIzaguirre <Agustinizag1@gmail.com>
2018-05-08 11:33:48 -0700

    New Logo

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -1,15 +1,7 @@
 #include <shell.h>
 #include <syscall.h>
 void printLogo();
-// #define LOGO_ASCI \
-//  _        _______  ______   _______  _______ 
-// ( (    /|(  ___  )(  __  \ (  ___  )(  ____ \\n
-// |  \  ( || (   ) || (  \  )| (   ) || (    \/\n
-// |   \ | || |   | || |   ) || |   | || (_____ \n
-// | (\ \) || |   | || |   | || |   | |(_____  )\n
-// | | \   || |   | || |   ) || |   | |      ) |\n
-// | )  \  || (___) || (__/  )| (___) |/\____) |\n
-// |/    )_)(_______)(______/ (_______)\_______)\n
+                                                                                     
 
 int main() {
      printLogo();
@@ -18,12 +10,30 @@ int main() {
 }
 
 void printLogo(){
-     printf("  _        _______  ______   _______  _______ \n");
-     printf(" ( (    /|(  ___  )(  __  \\ (  ___  )(  ____ \\\n");
-     printf(" |  \\  ( || (   ) || (  \\  )| (   ) || (    \\/\n");
-     printf(" |   \\ | || |   | || |   ) || |   | || (_____ \n");
-     printf(" | (\\ \\) || |   | || |   | || |   | |(_____  )\n");
-     printf(" | | \\   || |   | || |   ) || |   | |      ) |\n");
-     printf(" | )  \\  || (___) || (__/  )| (___) |/\\____) |\n");
-     printf(" |/    )_)(_______)(______/ (_______)\\_______)\n");
+printf("\n\n");
+printf("                                                                                                               \n");
+printf("                                                                 dddddddd                                      \n");
+printf("                          W E L C O M E    T O                   d::::::d     OOOOOOOOO        SSSSSSSSSSSSSSS \n");
+printf("                                                                 d::::::d   OO:::::::::OO    SS:::::::::::::::S\n");
+printf("                                                                 d::::::d OO:::::::::::::OO S:::::SSSSSS::::::S\n");
+printf("                                                                 d:::::d O:::::::OOO:::::::OS:::::S     SSSSSSS\n");
+printf("                  nnnn  nnnnnnnn       ooooooooooo       ddddddddd:::::d O::::::O   O::::::OS:::::S            \n");
+printf("                  n:::nn::::::::nn   oo:::::::::::oo   dd::::::::::::::d O:::::O     O:::::OS:::::S            \n");
+printf("                  n::::::::::::::nn o:::::::::::::::o d::::::::::::::::d O:::::O     O:::::O S::::SSSS         \n");
+printf("                  nn:::::::::::::::no:::::ooooo:::::od:::::::ddddd:::::d O:::::O     O:::::O  SS::::::SSSSS    \n");
+printf("                    n:::::nnnn:::::no::::o     o::::od::::::d    d:::::d O:::::O     O:::::O    SSS::::::::SS  \n");
+printf("                    n::::n    n::::no::::o     o::::od:::::d     d:::::d O:::::O     O:::::O       SSSSSS::::S \n");
+printf("                    n::::n    n::::no::::o     o::::od:::::d     d:::::d O:::::O     O:::::O            S:::::S\n");
+printf("                    n::::n    n::::no::::o     o::::od:::::d     d:::::d O::::::O   O::::::O            S:::::S\n");
+printf("                    n::::n    n::::no:::::ooooo:::::od::::::ddddd::::::ddO:::::::OOO:::::::OSSSSSSS     S:::::S\n");
+printf("                    n::::n    n::::no:::::::::::::::o d:::::::::::::::::d OO:::::::::::::OO S::::::SSSSSS:::::S\n");
+printf("                    n::::n    n::::n oo:::::::::::oo   d:::::::::ddd::::d   OO:::::::::OO   S:::::::::::::::SS \n");
+printf("                    nnnnnn    nnnnnn   ooooooooooo      ddddddddd   ddddd     OOOOOOOOO      SSSSSSSSSSSSSSS   \n");
+printf("                                                                                                               \n");
+printf("                                                                                                               \n");
+printf("                                                                                                               \n");
+printf("                                                                                                               \n");
+printf("                                                                                                               \n");
+printf("                                                                                                               \n");
+printf("                                                                                                               \n");
 }
